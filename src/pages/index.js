bc082ca10348c30018f571898520d9056c2f4ab3
Mathias <materbyl@gmail.com>
2019-06-29 11:36:21 +0200

    Made the header on the homepage responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
+import tw from 'tailwind.macro'
 
 import styled from 'styled-components'
 import logo from './../assets/Svartviken_logo_genomskinlig.png'
@@ -11,46 +12,39 @@ import '../styles/main.scss'
 import CampaignCardList from '../components/CampaignCardList'
 import AudioPlayerButton from '../components/AudioPlayerButton'
 
-const LeftColumn = styled.div`
-  height: 100%;
-  flex: 50%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-
-  h3 {
-    margin: 35px auto 5px auto;
-  }
+const HomeHeader = styled.header`
+  ${tw`bg-black text-white flex flex-row flex-wrap-reverse p-4`};
 `
 
-const HomeHeader = styled.div`
-  background: linear-gradient(
-    119.36deg,
-    #0a0a0a 0%,
-    #1d1d1d 99.99%,
-    #1d1d1d 100%
-  );
-  display: flex;
-  height: 850px;
-  flex-direction: row;
-  flex-wrap: wrap;
-  background: linear-gradient(
-    119.36deg,
-    #0a0a0a 0%,
-    #1d1d1d 99.99%,
-    #1d1d1d 100%
-  );
-  color: white;
+const LeftColumn = styled.div`
+  ${tw`sm:w-full sm:text-lg sm:text-center`};
+`
+
+const CampaignTitle = styled.h1`
+  ${tw`text-6xl`}
+`
+
+const CampaignDescription = styled.p`
+  ${tw`text-lg`}
+`
+const EpisodeTitle = styled.h1`
+  ${tw`text-2xl`}
+`
+
+const EpisodeDescription = styled.p`
+  ${tw`sm:text-base`}
 `
 
 const RightColumn = styled.div`
+  ${tw`sm:w-full`};
+
   height: 100%;
   flex: 50%;
   display: flex;
   flex-direction: column;
 
   img {
-    width: 600px;
+    width: 80%;
     -webkit-filter: drop-shadow(5px 5px 5px #222);
     filter: drop-shadow(5px 5px 5px #222);
     margin: 60px auto;
@@ -83,10 +77,7 @@ const RightColumn = styled.div`
 `
 
 const LatestEpisode = styled.div`
-  width: 800px;
-  margin: 0px 70px;
-  font-size: 24px;
-  color: ghostwhite;
+  ${tw`sm:text-center`}
 `
 
 class BlogIndex extends React.Component {
@@ -119,19 +110,15 @@ class BlogIndex extends React.Component {
         />
         <HomeHeader className="z-depth-3">
           <LeftColumn>
-            <LatestEpisode>
-              <h1>{latestCampaign.title}</h1>
-              <span
-                dangerouslySetInnerHTML={{
-                  __html: latestCampaign.description.description,
-                }}
-              />
-              <h3>{latestCampaign.episodes[0].title}</h3>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: latestCampaign.episodes[0].description.description,
-                }}
-              />
+            <LatestEpisode className="container text-center md:text-left">
+              <CampaignTitle>{latestCampaign.title}</CampaignTitle>
+              <CampaignDescription>
+                {latestCampaign.description.description}
+              </CampaignDescription>
+              <EpisodeTitle>{latestCampaign.episodes[0].title}</EpisodeTitle>
+              <EpisodeDescription>
+                {latestCampaign.episodes[0].description.description}
+              </EpisodeDescription>
 
               <AudioPlayerButton episode={latestCampaign.episodes[0]} />
             </LatestEpisode>
