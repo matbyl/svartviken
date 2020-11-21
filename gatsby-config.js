ff67e1bb5b39c28847d37e18d2742caff5e5889a
Mathias Frithiofsson <materbyl@gmail.com>
2020-11-21 17:52:45 +0100

    woops

--- gatsby-config.js
+++ gatsby-config.js
@@ -170,7 +170,7 @@ module.exports = {
     `gatsby-plugin-emotion`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-styled-components`
+    `gatsby-plugin-styled-components`,
     {
       resolve: 'gatsby-plugin-purgecss',
       options: {
