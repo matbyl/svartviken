343f0535ef0ea31c21464dd266d785616a5b8a7e
Mathias Frithiofsson <materbyl@gmail.com>
2021-07-30 18:02:27 +0200

    Revert tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   theme: {
-    fontSize: {
-      sm: ['14px', '20px'],
-      base: ['16px', '24px'],
-      lg: ['20px', '28px'],
-      xl: ['24px', '32px'],
-    }
   },
   variants: {},
   plugins: []
