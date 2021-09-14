e5be4318da862e0aef578805732b8cc41a68942e
Mathias Frithiofsson <materbyl@gmail.com>
2021-09-14 22:58:34 +0200

    fix about images

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,9 +11,6 @@ import Head from '../components/head'
 const AboutSection = styled.section`
   ${tw`flex flex-row flex-wrap justify-center w-full m-auto bg-white`};
 `
-const About = styled.section`
-  ${tw`w-full text-center text-xl m-auto`};
-`
 
 const PlayerSection = styled.div`
   ${tw`w-full flex flex-col md:flex-row p-12 md:even:flex-row-reverse even:bg-gray-200 justify-center`};
@@ -31,14 +28,6 @@ const CharacterName = styled.h1`
   ${tw`text-center text-2xl md:text-3xl font-bold`};
 `
 
-const CharacterInformation = styled.div`
-  ${tw`flex-auto`};
-`
-
-const CharacterDescription = styled.div`
-  ${tw`flex-auto text-gray-600 ml-4 my-auto`};
-`
-
 const CharacterPersonality = styled.h1`
   ${tw`flex-auto text-center font-semibold text-gray-700 border-t-2 border-gray-700 text-lg md:text-3xl`};
 `
@@ -50,14 +39,6 @@ const CharacterAttributes = styled.div`
 const Avatar = styled.img`
   ${tw`m-auto`};
 `
-
-const options = {
-  renderText: text => {
-    return text.split('\n').reduce((children, textSegment, index) => {
-      return [...children, index > 0 && <br key={index} />, textSegment]
-    }, [])
-  },
-}
 
 const commaSeperate = arr =>
   arr
@@ -75,7 +56,6 @@ export default class AboutPage extends React.Component {
           </HeaderContent>
         </Header>
         <AboutSection>
-          {/* <About>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</About> */}
           {this.props.data.allContentfulSvartvikenMember.edges.map(
             ({ node }) => (
               <PlayerSection key={node.id}>
@@ -163,7 +143,7 @@ export const pageQuery = graphql`
             iceBreakers
           }
           profilePicture {
-            fluid(maxWidth: 90) {
+            fluid(maxWidth: 1920) {
               src
             }
           }
