7c82347c7c045fb5678c8d45072292889e3fd70d
Mathmoose <materbyl@gmail.com>
2023-03-04 14:13:58 +0100

    Fix nix env for mac

--- shell.nix
+++ shell.nix
@@ -1,13 +1,14 @@
 let
-  nixpkgs_21_05 = import (fetchTarball
-    "https://github.com/NixOS/nixpkgs/archive/refs/tags/21.05.tar.gz") { };
-in nixpkgs_21_05.mkShell {
+  pkgs = import (fetchTarball { name = "nixpkgs-unstable";
+      url = "https://github.com/NixOS/nixpkgs/archive/3954218cf613eba8e0dcefa9abe337d26bc48fd0.tar.gz";
+      sha256 = "0bagqy53mknghz112swzb7zyjc07kfvam2wmc919bq0jxv8vj2i6";}) { };
+in pkgs.mkShell {
   buildInputs = [
-    nixpkgs_21_05.nodejs-12_x
-    nixpkgs_21_05.yarn
-    nixpkgs_21_05.nodePackages.gatsby-cli
-    nixpkgs_21_05.nodePackages.prettier
-    nixpkgs_21_05.nixfmt
+    pkgs.nodejs-14_x
+    pkgs.yarn
+    pkgs.nodePackages.gatsby-cli
+    pkgs.nodePackages.prettier
+    pkgs.nixfmt
   ];
 
   shellHook = ''
